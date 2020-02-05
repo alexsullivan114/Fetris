53007b816c72ad8c15af9bb6032e333d8222abea
Alex Sullivan <alexsullivan114@gmail.com>
2020-02-05 09:18:09 -0500

    Save theme when its selected

--- fetris/lib/main.dart
+++ fetris/lib/main.dart
@@ -1,24 +1,54 @@
+import 'dart:convert';
+
 import 'package:fetris/FetrisColorTheme.dart';
 import 'package:fetris/streams/SelectedTheme.dart';
+import 'package:fetris/utils/serialization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'Drawer.dart';
 import 'game/TetrisBoard.dart';
 
-void main() {
-  selectedTheme.listen((theme) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: theme.backgroundColor,
-      statusBarBrightness: theme.isDark ? Brightness.light : Brightness.dark,
-      statusBarIconBrightness:
-          theme.isDark ? Brightness.light : Brightness.dark,
-      systemNavigationBarColor: theme.backgroundColor,
-      systemNavigationBarIconBrightness:
-          theme.isDark ? Brightness.light : Brightness.dark,
-    ));
-  });
+void main() async {
   runApp(MyApp());
+  await restoreTheme();
+  selectedTheme.listen((theme) async {
+    saveTheme(theme);
+    updateSystemChrome(theme);
+  });
+}
+
+Future<void> restoreTheme() async {
+  final preferences = await SharedPreferences.getInstance();
+  if (preferences.containsKey("theme")) {
+    print("Theme in preferences: ${await preferences.get("theme")}");
+    Map<String, dynamic> themeMap = jsonDecode(await preferences.get("theme"));
+    print("Decoded theme: $themeMap");
+    final restoredTheme = deserializeTheme(themeMap);
+    selectedThemeSubject.value = restoredTheme;
+    print("Wahoo we're here!");
+  }
+
+  return;
+}
+
+void saveTheme(FetrisColorTheme theme) async {
+  final preferences = await SharedPreferences.getInstance();
+  await preferences.setString("theme", serializeTheme(theme));
+  print("Saving theme...");
+}
+
+void updateSystemChrome(FetrisColorTheme theme) {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: theme.backgroundColor,
+    statusBarBrightness: theme.isDark ? Brightness.light : Brightness.dark,
+    statusBarIconBrightness:
+    theme.isDark ? Brightness.light : Brightness.dark,
+    systemNavigationBarColor: theme.backgroundColor,
+    systemNavigationBarIconBrightness:
+    theme.isDark ? Brightness.light : Brightness.dark,
+  ));
 }
 
 class MyApp extends StatelessWidget {
---
+++ fetris/lib/utils/serialization.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'dart:ui';
+
+import 'package:fetris/FetrisColorTheme.dart';
+
+String serializeColor(Color color) {
+  final colorMap = {
+    "a": color.alpha,
+    "r": color.red,
+    "b": color.blue,
+    "g": color.green,
+  };
+
+  return jsonEncode(colorMap);
+}
+
+Color deserializeColor(Map<String, dynamic> colorMap) {
+  return Color.fromARGB(colorMap["a"], colorMap["r"], colorMap["b"], colorMap["g"]);
+}
+
+String serializeTheme(FetrisColorTheme theme) {
+  final themeMap = {
+    "t": serializeColor(theme.t),
+    "straight": serializeColor(theme.straight),
+    "square": serializeColor(theme.square),
+    "l": serializeColor(theme.l),
+    "s": serializeColor(theme.s),
+    "grid": serializeColor(theme.grid),
+    "gridLine": serializeColor(theme.gridLine),
+    "backgroundColor": serializeColor(theme.backgroundColor),
+    "accentColor": serializeColor(theme.accentColor),
+    "isDark": theme.isDark,
+  };
+
+  return jsonEncode(themeMap);
+}
+
+FetrisColorTheme deserializeTheme(Map<String, dynamic> themeMap) {
+  print("Our theme map: $themeMap");
+  return FetrisColorTheme(
+    t: deserializeColor(json.decode(themeMap["t"])),
+    straight: deserializeColor(json.decode(themeMap["straight"])),
+    square: deserializeColor(json.decode(themeMap["square"])),
+    l: deserializeColor(json.decode(themeMap["l"])),
+    s: deserializeColor(json.decode(themeMap["s"])),
+    grid: deserializeColor(json.decode(themeMap["grid"])),
+    gridLine: deserializeColor(json.decode(themeMap["gridLine"])),
+    backgroundColor: deserializeColor(json.decode(themeMap["backgroundColor"])),
+    accentColor: deserializeColor(json.decode(themeMap["accentColor"])),
+    isDark: themeMap["isDark"]);
+}
